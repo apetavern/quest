6e5f24db6830ed1397cb485ca465fa575e1a0720
Brian Lindenhof <brian@shadowbrain.net>
2022-07-05 14:45:14 +0200

    Quick little jiggle routine added to the mining state

--- code/Systems/States/MiningState.cs
+++ code/Systems/States/MiningState.cs
@@ -13,12 +13,23 @@ public partial class MiningState : State
 	[Net] public TimeUntil MiningTime { get; set; }
 	[Net] public bool ShouldMine { get; set; } = false;
 
+
+	TimeSince TimeSinceLastHit { get; set; }
+
+	bool DoJiggle;
+
+	Vector3 startPosition;
+
+	Rotation startRotation;
+
 	public MiningState() { }
 
 	public MiningState( OreDeposit target, Entity owner )
 	{
 		Target = target;
 		Owner = owner;
+		startPosition = target.Position;
+		startRotation = target.Rotation;
 	}
 
 	public override void Simulate()
@@ -26,12 +37,32 @@ public partial class MiningState : State
 		DebugOverlay.ScreenText( Target.Name, 1 );
 		var player = Owner as QuestPlayer;
 
+		if ( Host.IsServer && ShouldMine && MathF.Round( TimeSinceLastHit * 10f ) / 10f == 0.3f )
+		{
+			float RFloat = Rand.Float( -2f, 2f );
+			Target.Position += new Vector3( RFloat, RFloat, 0f );
+			Target.Rotation += new Angles( RFloat * 0.1f, 0, RFloat * 0.1f ).ToRotation();
+		}
+
+		if ( Host.IsServer && ShouldMine && TimeSinceLastHit > 1f )
+		{
+			TimeSinceLastHit = !DoJiggle ? -1f : 0f; //Has to wait a second extra because of animation start delay
+			DoJiggle = true;
+		}
+
+		if ( Host.IsServer && ShouldMine )
+		{
+			Target.Position = Vector3.Lerp( Target.Position, startPosition, 0.5f );
+			Target.Rotation = Rotation.Lerp( Target.Rotation, startRotation, 0.5f );
+		}
+
 		if ( MiningTime <= 0f && ShouldMine )
 		{
 			if ( Host.IsServer )
 			{
 				player.Inventory.AddItem( new Ore() );
 				player.Skills.AddExperience( Skills.SkillType.skill_mining, 10 );
+				DoJiggle = false;
 			}
 
 			Done = true;
@@ -51,7 +82,7 @@ public partial class MiningState : State
 		else
 		{
 			ShouldMine = true;
-			MiningTime = 3f;
+			MiningTime = 3.5f;
 		}
 	}
 
